5f28164bd90ff57579ffffcac8c3fe985ea7785e
taetoo <rlaougs5@naver.com>
2023-08-31 16:15:07 +0900

    다양한 서비스 로직 추가

--- src/main/java/com/example/springtest/service/PostService.java
+++ src/main/java/com/example/springtest/service/PostService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -21,18 +22,30 @@ import java.util.List;
 public class PostService {
     private final PostRepository postRepository;
 
-    // 전체 게시물 조회
+
+
+    // 전체 게시물 조회(stream)
     public ApiResult<List<PostResponseDto>> getMembers() {
-        List<Post> postAll = postRepository.findAllByOrderByModifiedAtDesc();
-        List<PostResponseDto> postResponseDtos = new ArrayList<>();
-        for(Post post : postAll){
-            PostResponseDto postResponseDto = new PostResponseDto(post);
-            postResponseDtos.add(postResponseDto);
-        }
-        return ApiUtils.success(postResponseDtos);
+        List<PostResponseDto> collect = postRepository.findAllByOrderByModifiedAtDesc().stream()
+                .map(PostResponseDto::new)
+                .collect(Collectors.toList());
+        return ApiUtils.success(collect);
+
+
     }
 
-    // 특정 게시물 조회
+    // 전체 게시물 조회(for each 문)
+//    public ApiResult<List<PostResponseDto>> getMembers() {
+//        List<PostResponseDto> postResponseDtos = new ArrayList<>();
+//        List<Post> postAll = postRepository.findAllByOrderByModifiedAtDesc();
+//        for (Post post : postAll) {
+//            PostResponseDto postResponseDto = new PostResponseDto(post);
+//            postResponseDtos.add(postResponseDto);
+//        }
+//        return ApiUtils.success(postResponseDtos);
+//    }
+
+     // 특정 게시물 조회
     public ApiResult<PostResponseDto> getMember(Long id) {
         Post post = postRepository.findById(id).orElseThrow(
                 () -> new CustomException(ErrorCode.NOT_FOUND_DATA)
@@ -40,6 +53,20 @@ public class PostService {
         PostResponseDto responseDto = new PostResponseDto(post);
         return ApiUtils.success(responseDto);
     }
+
+    // 특정 게시물 조회(DB 조회 메서드 별도 생성)
+//    public ApiResult<PostResponseDto> getMember(Long id) {
+//        Post post = checkMember(id);
+//        PostResponseDto responseDto = new PostResponseDto(post);
+//        return ApiUtils.success(responseDto);
+//    }
+//
+//    private Post checkMember(Long id){
+//       return postRepository.findById(id).orElseThrow(() -> {
+//            throw new IllegalArgumentException("게시물 정보가 없습니다.");
+//        });
+//    }
+
 
     // 게시물 등록
     public ApiResult<PostResponseDto> summitPost(PostRequestDto requestDto) {
@@ -56,12 +83,12 @@ public class PostService {
         Post post = postRepository.findById(id).orElseThrow(
                 () -> new CustomException(ErrorCode.NOT_FOUND_DATA)
         );
-        if(post.getPassword().equals(requestDto.getPassword())){
+        if (post.getPassword().equals(requestDto.getPassword())) {
             post.updatePost(requestDto);
             PostResponseDto responseDto = new PostResponseDto(post);
             return ApiUtils.success(responseDto);
         } else {
-            return ApiUtils.error("비밀번호를 확인해주세요.",444);
+            return ApiUtils.error("비밀번호를 확인해주세요.", 444);
         }
 
 
@@ -73,11 +100,11 @@ public class PostService {
         Post post = postRepository.findById(id).orElseThrow(
                 () -> new CustomException(ErrorCode.EMPTY_CLIENT)
         );
-        if(post.getPassword().equals(requestDto.getPassword())){
+        if (post.getPassword().equals(requestDto.getPassword())) {
             postRepository.deleteById(id);
-           return ApiUtils.success("삭제완료!");
-        } else{
-            return ApiUtils.error("비밀번호를 확인해주세요.",444);
+            return ApiUtils.success("삭제완료!");
+        } else {
+            return ApiUtils.error("비밀번호를 확인해주세요.", 444);
         }
     }
 
